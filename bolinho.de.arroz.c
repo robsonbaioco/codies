d88ee83ae1d269f96704078c570b94328034d4bc
Robson Samir Baioco <robson_baioco@hotmail.com>
2018-08-26 19:21:49 -0300

    add bolinho.de.arroz.c
    program that "sings" the music bolinho de arroz

---
+++ bolinho.de.arroz.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <windows.h>
+#include <conio.h>
+#include <stdlib.h>
+int main() {
+	int n=10000;
+	int i;
+    for(i=0;i<n;i++){
+    	printf("Eu sou um bolinho de arroz \n");
+    	Sleep(950);
+    	printf("(de arroz!) \n");
+    	Sleep(950);
+    	system("cls");
+    	printf("Meus bracinhos vieram so depois \n");
+    	Sleep(950);
+    	printf("(so depois!) \n");
+    	Sleep(950);
+    	system("cls");
+    	printf("minhas perninhas ainda esto por vir\n");
+    	Sleep(950);
+    	printf("(estao por vir!) \n");
+    	Sleep(950);
+    	system("cls");
+    	printf("e eu nao tenho boquinha pra sorrir\n");
+    	Sleep(950);
+    	system("cls");
+    	printf("porque? ");
+    	Sleep(600);
+    	printf("porque?\n");
+    	Sleep(950);
+    	system("cls");
+    	printf("porque\n");
+    }
+}
