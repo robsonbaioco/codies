900010344b4668743c3077806ca292d66b91f10f
Robson Samir Baioco <robson_baioco@hotmail.com>
2019-10-22 11:31:12 -0300

    show all the colors available on windows OS for C programs

---
+++ cores.c
@@ -0,0 +1,27 @@
+# include <stdio.h>
+# include <stdlib.h>
+# include <conio.h>
+
+int main(){
+system ("cls"); // limpa a tela
+system ("color 0B"); // cor de fundo preta e letras azuis brilhantes
+printf ("\n\n\t\t\tTESTE: AZUL");
+getch();
+system ("cls"); // limpa a tela
+system ("color 4C"); // cor de fundo vermelha e letras vermelhas brilhantes
+printf ("\n\n\t\t\tTESTE: VERMELHO");
+getch();
+system ("cls"); // limpa a tela
+system ("color 5D"); // cor de fundo roxa e letras lilases brilhantes
+printf ("\n\n\t\t\tTESTE: LILAS");
+getch();
+system ("cls"); // limpa a tela
+system ("color E0"); // cor de fundo preta e letras amarelas brilhantes
+printf ("\n\n\t\t\tTESTE: AMARELO");
+getch();
+system ("cls"); // limpa a tela
+system ("color 1B"); // cor de fundo azul e letras azuis claras
+printf ("\n\n\n");
+system ("color /? |more");
+getch();
+}
