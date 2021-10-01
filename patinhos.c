f52ebca683bc8e902f690326d4472d9af677a9d0
wilbertdener <denerwilbert@gmail.com>
2021-10-01 19:56:57 +0000

    Update patinhos for rolinhas

--- patinhos.c
+++ patinhos.c
@@ -3,41 +3,41 @@
 
 main(){
 	int n, m,i,j;
-	printf("Digite quantos patinhos foram passear \n");
+	printf("Digite quantas rolinhas foram passear \n");
 	scanf("%d", &n);
 	system("cls");
 	m=n;
 	for(i=m; i>1; i--){
-		printf("%d patinhos foram passear \n", n);
+		printf("%d rolinhas foram passear \n", n);
 		Sleep(1000);
 		printf("alem das montanhas para brincar \n");
 		Sleep(1000);
 		printf("a mamae gritou \n");
 		Sleep(1000);
 		for(j=0; j<4; j++){
-			printf("\a Qua! \n");
+			printf("\a Quero café! \n");
 			Sleep(500);
 		}
 		n--;
 		if(n>1){
 			
 		}else{
-			printf("mas 1 patinho voltou de la \n", n);
+			printf("mas só 1 rolinha voltou de la \n", n);//porque a variavel "n" se n printa? kkkk
 		}
 		Sleep(1500);
 		system ("cls");
 	}
-	printf(" %d patinho foi passear \n", n);
+	printf(" %d rolinha foi passear \n", n);
 	Sleep(1000);
 	printf("alem das montanhas para brincar \n");
 	Sleep(1000);
 	printf("a mamae gritou \n");
 	Sleep(1000);
 	for(j=0; j<4; j++){
-		printf("\a Qua! \n");
+		printf("\a Quero café! \n");
 		Sleep(500);
 	}
 	n--;
-	printf("mas nenhum patinho voltou de laaaaa");
+	printf("mas nenhuma caralha voltou de laaaaa");
 	Sleep(100000000);
 }
